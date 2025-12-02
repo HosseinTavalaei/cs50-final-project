e1f304d72222ff22cbe3af8580c36476396e6747
Hossein tavalaei <greathosseinchi@gmail.com>
2025-12-02 21:45:05 +0330

    create a secret key

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = '5d4b4a1acdf7c0e4b22f47cef267c9be'
+
 Session(app)
 
 db = SQL("sqlite:///todo.db")
