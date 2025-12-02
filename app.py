6a31417223ef14173a04ab04c87786484c73b8b4
Hossein tavalaei <greathosseinchi@gmail.com>
2025-12-02 22:30:25 +0330

    set register route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from src import app  
 from src import db   
+import src.forms
 import src.routes
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,23 @@
 from src import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
+from src.forms import RegistrationForm, LoginForm
 
 
-@app.route('/')
-def home():
-    return render_template('register.html')
+# @app.route('/')
+# def home():
+#     form = RegistrationForm()
+#     return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'The user created for {form.email.data}!', 'success')
+        # return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
-
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
